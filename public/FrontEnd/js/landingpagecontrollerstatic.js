46ef0255b9a3a9a1bd3264611a3052d667a9ad7d
SatabdiG <satabdi.ganguly89@gmail.com>
2017-02-08 17:06:09 +0100

    Git Changes

--- public/FrontEnd/js/landingpagecontrollerstatic.js
+++ public/FrontEnd/js/landingpagecontrollerstatic.js
@@ -9,6 +9,42 @@ function landingpagecontroller() {
     $(document).ready(function () {        
         // Initialize Masonry
         initializeGrid();
+        document.addEventListener("mousewheel", MouseWheelHandler, false);
+        document.addEventListener("DOMMouseScroll", MouseWheelHandler, false);
+        document.addEventListener("scroll", MouseWheelHandler, false);
+        //Initialize the parallax scrolling
+        $('a[href*=#]').each(function(){
+           if(location.pathname.replace(/^\//,"") === this.pathname.replace(/^\//,""))
+           {
+
+               var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+               var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+               if ($target) {
+                   var targetOffset = $target.offset().top;
+                   var dis=targetOffset-30;
+                   $(this).click(function(event) {
+                       var currselection=$(event.target).text();
+                       if(currselection !== "Top")
+                       {
+                           console.log("In target");
+                           $('#nav').removeClass('navbar-transparent');
+
+                       }else
+                       {
+                           $('#nav').addClass('navbar-transparent');
+                       }
+                       $("#nav li a").removeClass("active");
+                       $(this).addClass('active');
+                       $('html, body').animate({scrollTop: dis}, 1000);
+                       return false;
+                   });
+               }
+           }
+
+
+
+
+        });
 
     });
 }
@@ -23,6 +59,21 @@ function initializeGrid() {
       transitionDuration: 0
   });
 }
+
+function MouseWheelHandler(e)
+{
+    console.log("Mouse wheel detected");
+    //Remove class from navbar
+    var e= window.event || e;
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+   var pos=document.documentElement.scrollTop || document.body.scrollTop;
+   if(pos<56)
+        $("#nav").addClass("navbar-transparent");
+   else
+       $("#nav").removeClass("navbar-transparent");
+
+}
+
 
 
 landingpagecontroller();
